4411010b419aa4593d2760dba5ad04d7b5859e3f
Ayah Dahmani <ayadah@alumn.us.es>
2026-06-05 00:03:54 +0200

    second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name="TifliGPT.py",
+      version="1.0",
+      py_modules=["TifliGPT"],
+      description="TifliGPT is a simple implementation of pidiatic chatbot in Python.",
+      author="Aya Dahmani",
+      author_email="aya.dahmani@adiot.es",
+      url="http://www.example.com/tifligpt",
+      license="BSD style"
+      )
